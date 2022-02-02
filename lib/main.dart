026b577ad23c37dfb0f0148d850413f48b46bcdc
12345 <suvarna.bole8@gmail.com>
2022-02-02 17:21:54 +0530

    intial commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+//import 'package:flutter/material.dart';
+import 'package:navigation/screens/homepage.dart';
+
+void main() =>
+    runApp(MaterialApp(debugShowCheckedModeBanner: false, home: HomePage()));
